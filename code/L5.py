7f4fb68b9887925b777424227455acd983d71d0f
CTHA14 <163749446+CTHA14@users.noreply.github.com>
2024-10-29 23:50:04 +0500

    Delete code directory

--- code/L5.py
+++
@@ -1,6 +0,0 @@
-with open('input.txt','a+') as f:
-    f.write('\nAdd line')
-
-with open('input.txt','r') as f:
-    result=f.readlines()
-    print(result)
--- code/S3.py
+++
@@ -1,6 +0,0 @@
-with open('input1.txt') as f:
-	txt = f.read()
-	print('Input file contains:')
-	print(sum(map(str.isalpha, txt)), 'letters')
-	print(len(txt.split()), 'words')
-	print(txt.count('\n') + 1, 'lines')
