c1f9ed62113b5d5b0629a966dbbcdd8333a9782a
CTHA14 <163749446+CTHA14@users.noreply.github.com>
2024-11-05 23:39:26 +0500

    Delete code directory

--- code/L1.py
+++
@@ -1,6 +0,0 @@
-class car: #Создание класса
-    def __init__(self, manufac, model): #Создание атрибутов класса
-        self.manufac=manufac 
-        self.model=model 
-
-my_car=car("Audi", "A6") #Создание экземпляра класса
--- code/S1.py
+++
@@ -1,6 +0,0 @@
-class Team:
-    def __init__(self, mate1, mate2):
-        self.mate1=mate1
-        self.mate2=mate2
-
-rb_team=Team('Verstappen', 'Perez')
