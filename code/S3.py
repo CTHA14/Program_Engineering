d79b4065d2547a23b1bf98d38820f5e15096aa6c
CTHA14 <163749446+CTHA14@users.noreply.github.com>
2024-11-24 18:26:28 +0500

    Delete code directory

--- code/S3.py
+++
@@ -1,8 +0,0 @@
-def func(value):
-    try:
-        numb = int(value)
-        return numb*numb 
-    except ValueError:
-        print("Неподходящий тип данных. Ожидалось число.")
-print(func("aaAAaa"))
-print(func(33))
